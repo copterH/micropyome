55615e68e67b3ec2239ef85ac7e0e97a62a70527
eli <eli.desnyder@gmail.com>
2026-09-25 16:02:25 +0200

    Importation des données

--- CHLOELAN/data_explo.py
+++ CHLOELAN/data_explo.py
@@ -4,4 +4,14 @@ Created on Fri Sep 25 15:05:41 2026
 
 @author: edes
 """
+# %% Importation des librairies
 
+
+import pandas as pd
+
+
+# %% Importation des données avec pandas
+
+fam_fungi = pd.read_csv('observed.csv', sep=',')
+env13 = pd.read_csv('13_variables.csv', sep=',')
+env09 = pd.read_csv('09_variables.csv', sep=',')
